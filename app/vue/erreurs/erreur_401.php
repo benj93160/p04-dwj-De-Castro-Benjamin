936481376397481af57bb43f482520470ec4d4f3
benj93160 <benj93160@gmail.com>
2018-06-06 13:31:54 +0200

    Ajout des vues d'erreur liée à la connection de l'administration

---
+++ app/vue/erreurs/erreur_401.php
@@ -0,0 +1,8 @@
+<header class="entete vertical">
+    <!--<div class="opacity"></div>-->
+    <div class="row">
+        <div class="col-lg-12">
+            <h1 class="animated flipInX">Vous n'avez pas accès entré le bon mdp ou le bon login !</h1>
+        </div>
+    </div>
+</header>
---
+++ app/vue/erreurs/erreur_500.php
@@ -0,0 +1,8 @@
+<header class="entete vertical">
+    <!--<div class="opacity"></div>-->
+    <div class="row">
+        <div class="col-lg-12">
+            <h1 class="animated flipInX">Vous n'avez pas accès à cette partie du site !</h1>
+        </div>
+    </div>
+</header>
